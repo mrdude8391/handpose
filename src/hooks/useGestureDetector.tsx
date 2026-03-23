84ebc4dcbdda336202015cd089deb457cb3555f7
Bryan_w11 <h.bryan68@yahoo.ca>
2026-03-23 13:11:46 -0400

    commenting

--- src/hooks/useGestureDetector.tsx
+++ src/hooks/useGestureDetector.tsx
@@ -37,8 +37,8 @@ export const useGestureDetector = ({
   const detectorRef = useRef<handPoseDetection.HandDetector | null>(null);
   const animationRef = useRef<number | null>(null);
 
-  // --- smoothing state ---
-  const smoothingState = useRef<Record<string, MaxVotingState>>({
+  // --- smoothing ref ---
+  const smoothingRef = useRef<Record<string, MaxVotingState>>({
     Left: { history: [], counts: {} },
     Right: { history: [], counts: {} },
   });
@@ -46,7 +46,7 @@ export const useGestureDetector = ({
   const WINDOW_SIZE = 20;
 
   const smoothPrediction = (prediction: string, handedness: string) => {
-    const state = smoothingState.current[handedness];
+    const state = smoothingRef.current[handedness];
 
     state.history.push(prediction);
     state.counts[prediction] = (state.counts[prediction] || 0) + 1;
@@ -61,8 +61,8 @@ export const useGestureDetector = ({
     )[0];
   };
 
-  // --- combo state ---
-  const comboState = useRef<Record<string, ComboState>>({
+  // --- combo ref ---
+  const comboRef = useRef<Record<string, ComboState>>({
     Left: { countdown: 0, comboIdx: 0 },
     Right: { countdown: 0, comboIdx: 0 },
   });
@@ -70,7 +70,8 @@ export const useGestureDetector = ({
   const combo = ["Point Up", "Point Left"];
 
   const detectCombo = (gesture: string, handedness: string) => {
-    const state = comboState.current[handedness];
+    const state = comboRef.current[handedness];
+    // console.log(gesture, (state.countdown - Date.now()), state.comboIdx);
 
     if (state.comboIdx >= combo.length) {
       state.comboIdx = 0;
@@ -78,35 +79,49 @@ export const useGestureDetector = ({
       handleChangeImageIdx();
     }
 
+    // if we are neutral and user inputs combo start we initiate the coundown and move idx forward
     if (state.countdown <= Date.now() && gesture === combo[0]) {
-      state.countdown = Date.now() + 2000;
+      state.countdown = Date.now() + 2000; // 2 seconds
       state.comboIdx = 1;
     }
 
     if (state.countdown > Date.now()) {
+      // countdown is started and we are looking for the next move
       if (gesture === combo[state.comboIdx]) {
+        // if user inputs the next move, we can reset the countdown and move idx forward
         state.countdown = Date.now() + 2000;
         state.comboIdx += 1;
       } else if (gesture !== combo[state.comboIdx - 1]) {
+        // if the user inputs some gesture that isn't the next move
+        // and the gesture is not the current move (user holding the same move)
+        // i.e its a move outside of the combo sequence, we will reset the state
         state.comboIdx = 0;
-      }
+      } // if the user is just holding the current move then we don't need to do anything and just le the countdown do its thing
     } else {
+      // the coundown has run out, so we reset the combo state
       state.comboIdx = 0;
     }
   };
 
   const createDetector = async () => {
     if (!detectorRef.current) {
-      const model = handPoseDetection.SupportedModels.MediaPipeHands;
-
-      detectorRef.current = await handPoseDetection.createDetector(model, {
-        runtime: "mediapipe",
-        solutionPath: "node_modules/@mediapipe/hands",
-        modelType: "full",
-        maxHands: 2,
-      });
-
-      console.log("Detector created");
+      try {
+        const model = handPoseDetection.SupportedModels.MediaPipeHands;
+        const detectorConfig: handPoseDetection.MediaPipeHandsMediaPipeModelConfig =
+          {
+            runtime: "mediapipe",
+            solutionPath: "node_modules/@mediapipe/hands",
+            modelType: "full",
+            maxHands: 2,
+          };
+        detectorRef.current = await handPoseDetection.createDetector(
+          model,
+          detectorConfig,
+        );
+        console.log("Detector created");
+      } catch (error) {
+        console.error("Failed to create detector", error);
+      }
     }
   };
 
@@ -114,9 +129,13 @@ export const useGestureDetector = ({
     const webcam = webcamRef.current;
     const canvas = canvasRef.current;
 
+    // check for: webcam, canvas, and detector exists
     if (!webcam || !canvas || !detectorRef.current) return;
 
+    // check webcam has enough video for inference
     if (webcam.video?.readyState !== 4) {
+      // if not then we queue renderloop to run on next animation frame
+      // this will cause the render loop to run indefinitely (until isDetecting changes and causes useEffect to run)
       animationRef.current = requestAnimationFrame(renderLoop);
       return;
     }
@@ -130,37 +149,44 @@ export const useGestureDetector = ({
 
     const result: HandGesture = { left: "", right: "" };
 
+    // this array will have 1 hand item or 2 hand items,
+    // but the left/right being 1st or 2nd will change depending on detection order
     if (hands.length > 0) {
       initializeCanvas(canvasRef);
 
       hands.forEach((hand) => {
         drawHand(hand);
 
-        const estimated = estimateGestures(hand.keypoints3D as any);
-
+        const estimated = estimateGestures(
+          hand.keypoints3D as handPoseDetection.Keypoint[],
+        );
+        // swap handedness since webcam is not flipped
         const handedness = hand.handedness === "Left" ? "Right" : "Left";
-
+        // smooth out estimates using max vote technique
         const smooth = smoothPrediction(estimated.name, handedness);
-
+        // input the clean gesture signal to look for combo
         detectCombo(smooth, handedness);
 
         result[handedness.toLowerCase()] = smooth;
       });
       console.log(result);
     }
-
+    // update frontend
     handleChangeHandGesture(result);
 
+    // request animation and update the current animationref
     animationRef.current = requestAnimationFrame(renderLoop);
   };
 
+  // after mounting call this useEffect
   useEffect(() => {
     if (!isDetecting) {
-      console.log("Detector Dispose");
       detectorRef.current?.dispose();
       detectorRef.current = null;
+      console.log("Detector Disposed");
 
       if (animationRef.current) {
+        // cancel the next queued animation frame
         cancelAnimationFrame(animationRef.current);
       }
       return;
@@ -174,6 +200,7 @@ export const useGestureDetector = ({
     start();
 
     return () => {
+      // on unmount -> dispose of the detector and cancel queued animation frame
       detectorRef.current?.dispose();
       detectorRef.current = null;
 
